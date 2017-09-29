7249205731f3de559e650f0065e13168ad49d370
Sean <sean@Sens-MacBook-Pro.local>
2017-09-28 22:43:25 -0500

    added courses service

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,25 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}  
+            </li>
+        </ul>
+    ` // can also interpulate methods
 })
 export class CoursesComponent {
-
+    title = " List of courses";
+    courses;
+                // dependency injection .. need to include into providers in app module
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
+    // getTitle() {
+    //     return this.title;
+    // }
 }
