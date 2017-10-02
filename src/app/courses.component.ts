ab3b189387e8f1f0d9c5accffbdd0f3243fec9e0
Sean <sean@Sens-MacBook-Pro.local>
2017-10-02 17:41:02 -0500

    add custom pipe & favorite component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <input (keyup.enter)="onKeyUp()" />
+        {{ text | summary:25 }}
     ` // can also interpulate methods
 })
 export class CoursesComponent {
-    onKeyUp() {
-        console.log("Enter was pressed");
-    }
+    text = `
+        Lorem Ipsum simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.
+    `
+    
                 // class dependency injection .. need to include into providers in app module
     // constructor(service: CoursesService) {
     //     this.courses = service.getCourses();
@@ -24,3 +25,14 @@ export class CoursesComponent {
 // <button [style.backgroundColor="isActive ? 'blue' : 'white'"] --> style binding
 // <button (click)="onSave($event)" >Save</button> --> event binding
 // <input (keyup.enter)="onKeyUp()" /> --> event filtering
+// <input #email (keyup.enter)="onKeyUp(email.value)" /> --> template variables
+// <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> --> two way binding
+
+// PIPES
+// { { course.title | uppercase | lowercase } } <br/>
+// { { course.students | number } } <br/>
+// { { course.rating | number:'1.1-1' } } <br/>
+// { { course.price | currency:'AUD': true:'3.2-2' } } <br/>
+// { { course.releaseDate | date: 'shortDate' } }
+
+//CUSTOM PIPES
