2f6a30a4616bf9f44d30957ec8f9fbbd64b95329
Sean <sean@Sens-MacBook-Pro.local>
2018-01-17 19:16:32 -0600

    Map operator

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -17,10 +17,7 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
     this.service.getAll()
-      .subscribe(
-        response => {
-          this.posts = response.json();
-        });
+      .subscribe(posts => this.posts = posts);
   }
 
   createPost(input: HTMLInputElement) {
@@ -29,9 +26,9 @@ export class PostsComponent implements OnInit {
     
     this.service.create(post)
       .subscribe(
-        response => {
+        newPost => {
         //console.log(response.json());
-          post['id'] = response.json().id;
+          post['id'] = newPost.id;
           this.posts.splice(0, 0, post);
         }, 
         (error: AppError) => {
@@ -45,15 +42,15 @@ export class PostsComponent implements OnInit {
   updatePost(post) {
     this.service.update(post)
       .subscribe(
-        response => {
-          console.log(response.json());
+        updatePost => {
+          console.log(updatePost);
         });
   }
 
   deletePost(post) {
     this.service.delete(345)
       .subscribe(
-        response => {
+        () => {
           let index = this.posts.indexOf(post);
           this.posts.splice(index, 1);
         }, 
