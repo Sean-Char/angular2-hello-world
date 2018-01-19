89ca598bdf79fc19c7286526d091cbb246b8130c
Sean <sean@Sens-MacBook-Pro.local>
2018-01-18 20:28:00 -0600

    Create GitHub Followers Page

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubFollowersService } from '../github-followers.service';
 
 @Component({
-  selector: 'app-github-followers',
+  selector: 'github-followers',
   templateUrl: './github-followers.component.html',
   styleUrls: ['./github-followers.component.css']
 })
 export class GithubFollowersComponent implements OnInit {
+  followers: any[];
 
-  constructor() { }
+  constructor(private service: GithubFollowersService) { }
 
   ngOnInit() {
+    this.service.getAll()
+      .subscribe(followers => this.followers = followers);
   }
 
 }
